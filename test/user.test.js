67ba4d51e96280908361b38a173eeccc3be20cd9
Deo Kamara <deokamara@Deos-MacBook-Pro.local>
2018-10-15 15:07:23 +0300

    [Feature 160988055] Test signup endpoint

--- test/user.test.js
+++ test/user.test.js
@@ -1,4 +1,20 @@
+process.env.NODE_ENV = 'test';
 import User from '../api/models/user.model';
+import Server from '../api/server';
+const request = require('supertest')(Server);
+const user ={
+  username: 'io',
+  firstname: 'deo',
+  secondname: 'Kamara',
+  email: 'deo.kamara@andela.com',
+  password: '1234',
+};
+beforeAll(() => {
+  User.remove({}, (err) => {
+
+  });
+});
+
 test('test if user model is a function model', () => {
   expect(typeof(User)).toBe('function');
   expect(User.schema.obj.username).toBeTruthy();
@@ -6,4 +22,19 @@ test('test if user model is a function model', () => {
   expect(User.schema.obj.secondname).toBeTruthy();
   expect(User.schema.obj.password).toBeTruthy();
   expect(User.schema.obj.email).toBeTruthy();
+});
+test('test if user can signup', () => {
+  return request
+      .post('/ul/signup')
+      .send(user)
+      .then((response) => {
+        expect(response.status).toBe(201);
+      });
+});
+
+afterAll(() => {
+  Server.close( () => {
+    console.log( 'Closed out remaining connections.');
+  //  Close db connections, etc.
+  });
 });
